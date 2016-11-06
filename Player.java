3d4976a4edc81ab3afd3b2d819200c9ed2976aba
t00165396 <liranda.krasniqi@ittralee.ie>
2016-11-06 21:09:36 +0000

    Initial commit

---
+++ Player.java
@@ -0,0 +1,42 @@
+
+public class Player {
+	private String name;
+	private int score;
+
+	
+
+	public int getScore() { return score;}
+	public  String getName() { return name;}
+	
+
+	public void setScore (int score) {
+				this.score = score;
+	}
+	
+	public void setName( String name) {
+				this.name = name;
+	}
+	
+	
+	
+	public Player(String name, int score) {
+				setName(name);
+				setScore(score);
+			
+	}
+	
+	/** no-args constructor, for use as in Person p = new Person()
+	 *  to create a default Person
+	 */
+	public Player() {
+				this("Not Given",0);
+	}
+	
+		
+	/* Every object should have one of these, to give a quick
+	 * String summary of the values of all the object's attributes
+	 */
+	public String toString() {
+				return getName() + " " + getScore();
+	}
+}
---
+++ snake.java
@@ -0,0 +1,55 @@
+import java.awt.*;
+import java.util.LinkedList;
+
+
+
+/**
+ * Created by lirandakrasniqi on 06/11/2016.
+ */
+public class snake {
+
+    private LinkedList<Point> snake;//a linkedList with point objects
+    private Point snakesHead;
+    private Point snakesTail;
+
+    public snake(){
+
+        snake.addFirst(new Point(20,20));//  Returns the first element in this list.in this case it would be thehead of the snakeGame
+    }
+
+    public Point getSnakesHead(){
+        return  snake.getFirst();
+    }
+
+    public Point getSnakesTail(){
+        return  snake.getLast();
+
+    }
+
+
+
+    public void drawSnake(Graphics g) {
+        for (int i = 0; i < snake.size(); i++) {
+            g.setColor(Color.GREEN);
+            Point p = snake.get(i);
+            g.fillOval(p.x * 15, p.y * 15, 15, 15);
+        }
+    }
+
+    public void moveSnake(int dx, int dy) {
+        for (int i = snake.size() - 1; i >= 1; i--) {
+            snake.get(i).setLocation(snake.get(i - 1));
+        }
+        snake.getFirst().x += dx;
+        snake.getFirst().y += dy;
+    }
+
+
+    public void growSnake(int n) {
+        while (n > 0) {
+            Point tail = snake.getLast();
+            snake.add(new Point(tail));
+            n--;
+        }
+    }
+}
